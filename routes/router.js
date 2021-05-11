795d1451284a4e0c5482283ab294525565901f50
tanoxil <58617997+tanoxil@users.noreply.github.com>
2021-05-11 01:27:54 -0400

    test api stocks

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var connected = false;
+const data = yahooStockPrices.getCurrentData('AAPL');
+console.log(data); // { currency: 'USD', price: 132.05 }
+
 
 // GET route for reading data
 router.get('/', function (req, res, next) {
